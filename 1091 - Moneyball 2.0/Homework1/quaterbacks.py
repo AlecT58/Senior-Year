5bbf9be763f0060a7e9f2554827c7247b10472b4
atrievel <atrievel@gmail.com>
2018-01-30 12:59:29 -0500

    Refactored Code to be More Readable

--- 1091 - Moneyball 2.0/Homework1/quaterbacks.py
+++ 1091 - Moneyball 2.0/Homework1/quaterbacks.py
@@ -1,31 +1,46 @@
 import bs4 as bs
-import urllib.request
+import urllib.request as req
 
 class Quaterback:
-    passer_ratings = []
-
-    def __init__(self, name, passer_rating_list):
+    def __init__(self, name, reg_season_url, post_season_url):
         self.name = name
-        self.passer_ratings = passer_rating_list
-        self.career_passer_rating = self.find_career_passer_rating()
+        self.reg_season_url = reg_season_url
+        self.post_season_url = post_season_url
+        self.parse_pro_football_reference()
+        self.calculate_career_passer_ratings()
 
-    def find_career_passer_rating(self):
-        return "{0:.2f}".format(float(self.passer_ratings[-1].text))
+    #Use BeautifulSoup 4 to read the HTML, parse the file, and locate the cells with pass_rating statistics
+    def parse_pro_football_reference(self):
+        sauce_reg = req.urlopen(self.reg_season_url).read()
+        sauce_post = req.urlopen(self.post_season_url).read()
+        soup_reg = bs.BeautifulSoup(sauce_reg, 'lxml')
+        soup_post = bs.BeautifulSoup(sauce_post, 'lxml')
 
-def parse_data(url):
-    sauce = urllib.request.urlopen(url).read()
-    soup = bs.BeautifulSoup(sauce, 'lxml')
+        self.passer_ratings_reg = soup_reg.body.select('td[data-stat="pass_rating"]')
+        self.passer_ratings_post = soup_post.body.select('td[data-stat="pass_rating"]')
 
-    return soup.body.select('td[data-stat="pass_rating"]')
+    #Find the career passer ratings for the regualr season and post season from the list of all passer ratings
+    def calculate_career_passer_ratings(self):
+        self.reg_season_rating = "{0:.2f}".format(float(self.passer_ratings_reg[-1].text))
+        self.post_season_rating = "{0:.2f}".format(float(self.passer_ratings_post[-1].text))
+
+    #Compare two QBs, printing which has the better regular and post season ratings
+    def compare_passer_ratings(self, other_qb):
+        if self.reg_season_rating > other_qb.reg_season_rating:
+            print("[Reg. Season] {} is better based on his career passer rating of: {}".format(self.name, self.reg_season_rating))
+        else:
+            print("[Reg. Season] {} is better based on his career passer rating of: {}".format(other_qb.name, other_qb.reg_season_rating))
+
+        if self.post_season_rating > other_qb.post_season_rating:
+            print("[Post Season] {} is better based on his career passer rating of: {}".format(self.name, self.post_season_rating))
+        else:
+            print("[Post Season] {} is better based on his career passer rating of: {}".format(other_qb.name, other_qb.post_season_rating))
 
 def main():
-    ben = Quaterback("Ben Roethlisberger", parse_data('https://www.pro-football-reference.com/players/R/RoetBe00.htm'))
-    terry = Quaterback("Terry Bradshaw", parse_data('https://www.pro-football-reference.com/players/B/BradTe00.htm'))
+    ben = Quaterback("Ben Roethlisberger", 'https://www.pro-football-reference.com/players/R/RoetBe00.htm', 'https://www.pro-football-reference.com/players/R/RoetBe00/gamelog/post/')
+    terry = Quaterback("Terry Bradshaw", 'https://www.pro-football-reference.com/players/B/BradTe00.htm', 'https://www.pro-football-reference.com/players/B/BradTe00/gamelog/post/')
 
-    if ben.career_passer_rating > terry.career_passer_rating:
-        print("{} is better based on his career passer rating of: {}".format(ben.name, ben.career_passer_rating))
-    else:
-        print("{} is better based on his career passer rating of: {}".format(terry.name, terry.career_passer_rating))
+    ben.compare_passer_ratings(terry)
 
 if __name__ == "__main__":
     main()
