04e0b8c434141fdcfa621b7913c531d5010c2f16
atrievel <atrievel@gmail.com>
2018-01-30 13:20:10 -0500

    Added Question 3 Solution

---
+++ 1091 - Moneyball 2.0/Homework1/montecarlo.py
@@ -0,0 +1,24 @@
+import random
+
+def run_monte_carlo(times):
+    within_twenty = 0
+
+    for n in range(0, times):
+        alice = calculate_arrival_time()
+        bob = calculate_arrival_time()
+
+        if abs(alice - bob) <= 20:
+            within_twenty += 1
+
+    return "{0:.4f}".format(float(within_twenty / times))
+
+
+def calculate_arrival_time():
+    return random.randint(0, 120) + 1
+
+def main():
+    print(run_monte_carlo(10000))
+
+if __name__ == '__main__':
+    main()
+        
