2c03ee6c783f4e3cc62adbc9a4bc16e2755b9c0b
atrievel <atrievel@gmail.com>
2018-02-12 13:49:08 -0500

    Updated Assignment 2

--- 1632 - Quality Assurance/Exercises/2/WolfpackTest.rb
+++ 1632 - Quality Assurance/Exercises/2/WolfpackTest.rb
@@ -4,5 +4,16 @@ require_relative 'wolf'
 require_relative 'wolf_pack'
 
 class WolfPackTest < Minitest::Test
-  
+    def test_wolfpack_is_a_wolfpack
+        pack = WolfPack.new
+        assert pack.is_a?(WolfPack)
+    end
+
+
+
+
+    # def test_add_new_wolf
+    #     pack = wolf_pack.new
+    #     pack.add Wolf.new("Chuck", 60)
+    # end
 end
--- 1632 - Quality Assurance/Exercises/2/wolf_pack.rb
+++ 1632 - Quality Assurance/Exercises/2/wolf_pack.rb
@@ -10,23 +10,29 @@ class WolfPack
     # do nothing.
     def all_howl
       # TODO
+      # each, call howl
+      # @wolves.each |x| do
+      #   x.howl
+      # end
     end
   
     # Make one random wolf in pack bark by calling their
     # .bark method.  If there are no wolves, do nothing.
     def one_bark
       # TODO
+      # sample, call bark
+      # @wolves.sample |x| do
+      #   x.bark
+      # end
     end
   
     # Create a new wolf and add it to the pack
-    def add_wolf name, size
-      # TODO
+    def add_wolf wolf
+      # @wolves.push wolf
     end
   
     # Return the wolf with the largest size in @wolves
     def biggest_wolf
       # TODO
     end
-  
-    
   end
