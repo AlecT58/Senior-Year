2325c43e4279cd5ef78277b0f719b894abf8ad35
atrievel <atrievel@gmail.com>
2018-02-12 13:49:27 -0500

    Added Prokject 6 Files

---
+++ 0334 - Web Design/Project 6 - Modified Fizzbuzz/README.md
@@ -0,0 +1,8 @@
+# Project 6 for CS0334 Spring 2018 - Alec Trievel
+
+## Project Description and [Project Link](http://www.alectrievel.com/schoolwork/CS0334/project6/index.php)
+
+* Write a program that prints the numbers from 1 to 100; but for multiples of three, print "Go" instead of the number, and for multiples of five, print "Pitt".  For numbers which are multiples of both three and five, print "GoPitt".
+
+* Assume that you'll be working with integers only.
+
---
+++ 0334 - Web Design/Project 6 - Modified Fizzbuzz/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Alec Trievel">
+    <title>Go Pitt | Fizzbuzz</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+</head>
+<body>
+    <div class="text-center">
+    <h2 class='display-3'>"GoPitt" Fizzbuzz</h2>
+    <a class="btn btn-md btn-info my-3" href="http://www.alectrievel.com/schoolwork/CS0334/project6/modified_fizzbuzz.txt" download>Download PHP as Text</a>
+    <br>
+    <?php
+        for ($i = 1; $i <= 100; $i++)
+        {
+            $fizzy = (bool)($i % 3 == 0);
+            $buzzy = (bool)($i % 5 == 0);
+
+            if($fizzy && $buzzy){
+                echo "GoPitt<br />";
+            }
+            else if($fizzy){
+                echo "Go<br />";
+            }
+            else if($buzzy){
+                echo "Pitt<br />";
+            }
+            else {
+                echo $i."<br />";
+            }
+        }
+    ?>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+</html>
