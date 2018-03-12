aa646bdfb97771f8a164f333587bc34982a5fa98
Alec Trievel <atrievel@gmail.com>
2018-03-12 13:38:12 -0400

    Finished project 8

---
+++ 0334 - Web Design/Project 8 - Guestbook/logGuest.php
@@ -0,0 +1,30 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    
+    if(isset($_POST)) {
+        //get the data from the user and store in array
+        $guest_data = Array (trim(stripslashes($_POST['first_name'])), trim(stripslashes($_POST['last_name'])), trim(stripslashes($_POST['address'])),
+                             trim(stripslashes($_POST['school'])), trim(stripslashes($_POST['email'])), trim(stripslashes($_POST['phone'])),
+                             trim(stripslashes($_POST['birthday'])), trim(stripslashes($_POST['color'])), trim(stripslashes($_POST['comment']))
+        );
+
+        $csv =  generateCSV($guest_data);
+        echo $csv;
+    }
+
+    function generateCSV($data, $delimiter = ',', $enclosure = '"', $escape_char = "\n") {
+        $handle = fopen('guests.mas', 'a+');
+        fputcsv($handle, $data, $delimiter, $enclosure);
+        rewind($handle);
+        $json_list = Array();
+
+        while(!feof($handle))
+        {
+            array_push($json_list, json_encode(fgetcsv($handle)));
+        }
+        fclose($handle);
+
+        array_pop($json_list);
+        return json_encode($json_list);
+    }
+?>
