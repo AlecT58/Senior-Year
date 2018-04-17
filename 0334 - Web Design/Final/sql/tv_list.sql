6042a40c9aaef2c930d0a863c0b498af7a910c3e
Alec Trievel <atrievel@gmail.com>
2018-04-16 22:40:51 -0400

    Updated views to include user_id

--- 0334 - Web Design/Final/sql/tv_list.sql
+++ 0334 - Web Design/Final/sql/tv_list.sql
@@ -1,3 +1,3 @@
 CREATE VIEW Owned_TV_List AS 
-SELECT TV.show_name, TV.episode, TV.genre, TV.price 
+SELECT TV.show_name, TV.episode, TV.genre, TV.price, Owned_TV.user_id
 FROM TV JOIN Owned_TV ON TV.id=Owned_TV.tv_id
